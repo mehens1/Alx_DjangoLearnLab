570fe78ec1c354b8048ab7c1659f9acf654452cb
mehens1 <mehens1@gmail.com>
2024-08-17 20:31:04 +0100

    bug fix

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,48 +1,14 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 
-from django.contrib.auth import login
-from django.contrib.auth import logout
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
-from django.shortcuts import redirect
-from django.urls import reverse_lazy
-from django.contrib.auth.decorators import login_required
-
-# Login view
-def user_login(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('home')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'login.html', {'form': form})
-
-# Logout view
-def user_logout(request):
-    logout(request)
-    return render(request, 'logout.html')
-
-# Registration view
-def user_register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')
-    else:
-        form = UserCreationForm()
-    return render(request, 'register.html', {'form': form})
 
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', user_login, name='login'),
-    path('logout/', user_logout, name='logout'),
-    path('views.register', user_register, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
