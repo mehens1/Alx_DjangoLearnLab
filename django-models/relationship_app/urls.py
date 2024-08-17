cb335c02a25a496bd987223b2cc87adfa6ae3126
mehens1 <mehens1@gmail.com>
2024-08-17 20:43:15 +0100

    bug fix

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from . import views
 from .views import list_books, LibraryDetailView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
@@ -10,4 +11,5 @@ urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register', views.redirect)
 ]
