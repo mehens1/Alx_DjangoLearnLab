400b4a81e72d4c8853a6f5b2edbc2bef31b559b3
mehens1 <mehens1@gmail.com>
2024-08-17 20:04:04 +0100

    bug fix

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,8 +1,12 @@
-# relationship_app/views.py
-
 from django.shortcuts import render
-from .models import Book
+from django.views.generic.detail import DetailView
+from .models import Book, Library
 
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
