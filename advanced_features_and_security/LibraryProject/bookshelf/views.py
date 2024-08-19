f85284d636303022290aeed2063790df5b9b7733
mehens1 <mehens1@gmail.com>
2024-08-19 23:45:41 +0100

    second task

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import get_object_or_404
 from .models import Book
+from .forms import ExampleForm
 from .forms import BookForm
 
 @permission_required('app_name.can_view', raise_exception=True)
