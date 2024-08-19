1c321d8d92b553873fb876ec0ea05009cd8ab0b5
mehens1 <mehens1@gmail.com>
2024-08-19 23:35:18 +0100

    second task

--- advanced_features_and_security/LibraryProject/README.md
+++ advanced_features_and_security/LibraryProject/README.md
@@ -1,1 +1,18 @@
-python manage.py runserver
+# Permissions and Groups Setup
+
+## Custom Permissions
+In the `Book` model, the following custom permissions have been defined:
+- `can_view`: Allows viewing of books.
+- `can_create`: Allows creation of new books.
+- `can_edit`: Allows editing of existing books.
+- `can_delete`: Allows deletion of books.
+
+## User Groups
+Three groups have been created:
+- **Editors**: Can view, create, and edit books.
+- **Viewers**: Can only view books.
+- **Admins**: Can view, create, edit, and delete books.
+
+## Views Enforcement
+Permissions are enforced in the views using the `@permission_required` decorator. Only users with the appropriate permissions can access the respective views.
+
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, get_object_or_404
+from .models import Book
 
-# Create your views here.
+@permission_required('app_name.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'app_name/book_list.html', {'books': books})
+
+@permission_required('app_name.can_create', raise_exception=True)
+def book_create(request):
+    # Implementation for creating a book
+    pass
+
+@permission_required('app_name.can_edit', raise_exception=True)
+def book_edit(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    # Implementation for editing a book
+    pass
+
+@permission_required('app_name.can_delete', raise_exception=True)
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    # Implementation for deleting a book
+    pass
