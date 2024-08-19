1c8e79786c560324198e3c8eee2a9b55a980dcd7
mehens1 <mehens1@gmail.com>
2024-08-19 23:36:59 +0100

    second task

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -1,1 +1,15 @@
+import os
+
 AUTH_USER_MODEL = 'bookshelf.CustomUser'
+
+DEBUG = False
+ALLOWED_HOSTS = ['yourdomain.com', 'www.yourdomain.com']
+
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'your-secret-key')
