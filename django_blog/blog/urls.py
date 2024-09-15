8712c2bdf34831751408d7d311090c25e27cf9eb
mehens1 <mehens1@gmail.com>
2024-09-15 23:17:08 +0100

    fourth  project for week 14

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,7 +4,13 @@ from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, CommentCreateView, CommentUpdateView, CommentDeleteView
+from .views import (
+    PostListView, PostDetailView, PostCreateView,
+    PostUpdateView, PostDeleteView, CommentCreateView, 
+    CommentUpdateView, CommentDeleteView,
+    UserLoginView, UserLogoutView, register, profile
+)
+# from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, CommentCreateView, CommentUpdateView, CommentDeleteView
 
 urlpatterns = [
     path('login/', views.UserLoginView.as_view(), name='login'),
