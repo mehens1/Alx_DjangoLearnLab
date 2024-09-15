0803bf198fb217587f2c7ddf4a273861c324f56b
mehens1 <mehens1@gmail.com>
2024-09-15 23:06:07 +0100

    third project for week 14

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -17,8 +17,8 @@ urlpatterns = [
     path('', include('blog.urls')),  # Include blog app URLs
     
     path('', PostListView.as_view(), name='post-list'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),  # Add this line
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
