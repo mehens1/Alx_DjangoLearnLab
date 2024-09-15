4108b59deeb95471aa2c894507658c5afdc24b72
mehens1 <mehens1@gmail.com>
2024-09-15 23:02:33 +0100

    third project for week 14

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,11 +1,10 @@
-# blog/urls.py
-
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path('login/', views.UserLoginView.as_view(), name='login'),
@@ -16,4 +15,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='blog/home.html'), name='home'),
     path('', include('blog.urls')),  # Include blog app URLs
+    
+    path('', PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,16 +1,21 @@
 # blog/views.py
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
-from django.contrib.auth import login
-from django.contrib.auth import authenticate
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.decorators import login_required
 from .forms import RegisterForm, ProfileUpdateForm
 
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from .models import Post
+from .forms import PostForm
+
 class UserLoginView(LoginView):
     template_name = 'blog/login.html'
-
+    
 class UserLogoutView(LogoutView):
     template_name = 'blog/logout.html'
 
@@ -19,7 +24,6 @@ def register(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
-            # Optionally, log the user in after registration
             login(request, user)
             return redirect('profile')
     else:
@@ -32,10 +36,51 @@ def profile(request):
         form = ProfileUpdateForm(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
-            # Optionally, add a success message
             from django.contrib import messages
             messages.success(request, 'Your profile has been updated!')
             return redirect('profile')
     else:
         form = ProfileUpdateForm(instance=request.user)
     return render(request, 'blog/profile.html', {'form': form})
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'  # Default would be blog/post_list.html
+    context_object_name = 'posts'
+    ordering = ['-published_date']  # Order posts by published date, newest first
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # Set the current user as the author
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  # Ensure only the author can edit the post
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post-list')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  # Ensure only the author can delete the post
+
