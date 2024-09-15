9a5811bb376a425fb6357d842137b4f3515af08a
mehens1 <mehens1@gmail.com>
2024-09-15 22:47:52 +0100

    second project for week 14

---
+++ django_blog/blog/forms.py
@@ -0,0 +1,19 @@
+# blog/forms.py
+
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True, help_text='Required. Enter a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class ProfileUpdateForm(forms.ModelForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
