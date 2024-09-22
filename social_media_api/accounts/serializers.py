eb398da15c93402704c79e7855efa51f0cdb5011
mehens1 <mehens1@gmail.com>
2024-09-22 22:00:42 +0100

    first project attempt

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -6,8 +6,8 @@ from rest_framework.fields import CharField
 
 # Get the user model
 User = get_user_model()
+serializers.CharField()
 
-# Serializer for user registration
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
     password_confirm = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
@@ -16,23 +16,20 @@ class RegisterSerializer(serializers.ModelSerializer):
         model = User
         fields = ['username', 'email', 'password', 'password_confirm']
 
-    def validate(self, data):
-        if data['password'] != data['password_confirm']:
+    def validate_password_confirm(self, value):
+        if self.initial_data['password'] != value:
             raise serializers.ValidationError("Passwords do not match.")
-        return data
+        return value
 
     def create(self, validated_data):
         validated_data.pop('password_confirm')
-
         user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
         )
 
-        # Create a token for the new user
         Token.objects.create(user=user)
-
         return user
     
 class UserSerializer(serializers.ModelSerializer):
@@ -41,7 +38,7 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('id', 'username', 'email', 'first_name', 'last_name', 'bio')
 
 class TokenSerializer(serializers.ModelSerializer):
-    auth_token = serializers.CharField(source='key')  # Explicit CharField
+    auth_token = serializers.CharField(source='key')
 
     class Meta:
         model = Token
