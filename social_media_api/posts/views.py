4b03a847604d5ee2cf91958dfc34ee286f020634
mehens1 <mehens1@gmail.com>
2024-09-22 22:25:55 +0100

    second project attempt 2

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,56 +1,35 @@
-from rest_framework import viewsets, permissions, generics
-from django.shortcuts import get_object_or_404
-from .models import Post, Comment, Like
+from rest_framework import permissions
+from rest_framework import viewsets
+from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
+from rest_framework.permissions import IsAuthenticated
+from .models import Post
 from django.contrib.auth import get_user_model
-from rest_framework.response import Response
-from rest_framework.decorators import action
-from rest_framework import status
 
 User = get_user_model()
 
 class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-    def get_queryset(self):
-        user = self.request.user
-        # Get the users that the current user follows
-        followed_users = user.following.all()  
-        # Return posts from the followed users
-        return Post.objects.filter(author__in=followed_users).order_by('-created_at')
-
-    @action(detail=True, methods=['post'])
-    def like(self, request, pk=None):
-        post = get_object_or_404(Post, pk=pk)
-        user = request.user
-        # Check if the post is already liked
-        if not Like.objects.filter(user=user, post=post).exists():
-            Like.objects.create(user=user, post=post)
-            return Response({'status': 'post liked'}, status=status.HTTP_201_CREATED)
-        return Response({'status': 'post already liked'}, status=status.HTTP_400_BAD_REQUEST)
-
-    @action(detail=True, methods=['post'])
-    def unlike(self, request, pk=None):
-        post = get_object_or_404(Post, pk=pk)
-        user = request.user
-        # Check if the post is liked and remove the like
-        like = Like.objects.filter(user=user, post=post).first()
-        if like:
-            like.delete()
-            return Response({'status': 'post unliked'}, status=status.HTTP_200_OK)
-        return Response({'status': 'post not liked'}, status=status.HTTP_400_BAD_REQUEST)
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all().order_by('-created_at')
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-class FeedView(generics.ListAPIView):
-    serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
-    def get_queryset(self):
-        user = self.request.user
-        followed_users = user.following.all()  # Get the users that the current user follows
-        return Post.objects.filter(author__in=followed_users).order_by('-created_at')
+class FeedView(viewsets.ViewSet):
+    permission_classes = [IsAuthenticated]
+
+    def list(self, request):
+        user = request.user
+        following_users = user.following.all()
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
