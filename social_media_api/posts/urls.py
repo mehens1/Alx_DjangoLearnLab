dad47ba5eb5af00943806649b6ed52127a58b8e8
mehens1 <mehens1@gmail.com>
2024-09-22 22:32:06 +0100

    second project attempt 4

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -2,6 +2,7 @@ from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 from .views import PostViewSet, CommentViewSet
 from .views import FeedView
+from .views import UserFeedView
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
@@ -10,5 +11,6 @@ router.register(r'feed', FeedView, basename='feed')
 
 
 urlpatterns = [
+    path('feed/', UserFeedView.as_view(), name='user_feed'),
     path('', include(router.urls)),
 ]
