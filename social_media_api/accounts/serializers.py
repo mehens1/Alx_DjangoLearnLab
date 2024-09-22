7a0705a53173476b13eee9c222b8f4dbefacc5d9
mehens1 <mehens1@gmail.com>
2024-09-22 21:43:11 +0100

    first project attempt 2

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,45 +1,39 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from django.contrib.auth.password_validation import validate_password
 from rest_framework.authtoken.models import Token
 
+# Get the user model
 User = get_user_model()
 
-class RegisterSerializer(serializers.Serializer):
-    username = serializers.CharField(required=True)
-    email = serializers.CharField(required=True)
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)
-    first_name = serializers.CharField(required=True)
-    last_name = serializers.CharField(required=True)
-    bio = serializers.CharField(required=False)
+# Serializer for user registration
+class RegisterSerializer(serializers.ModelSerializer):
+    # CharField to handle password input securely
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+    password_confirm = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
 
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
-        return attrs
-
-    def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
-            password=validated_data['password']
-        )
-        user.bio = validated_data.get('bio', '')
-        user.save()
-        Token.objects.create(user=user)
-        return user
-
-class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'bio')
+        fields = ['username', 'email', 'password', 'password_confirm']
 
-class TokenSerializer(serializers.ModelSerializer):
-    auth_token = serializers.CharField(source='key')
+    # Validate that passwords match
+    def validate(self, data):
+        if data['password'] != data['password_confirm']:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
 
-    class Meta:
-        model = Token
-        fields = ('auth_token', 'created')
+    # Create a new user
+    def create(self, validated_data):
+        # Remove password_confirm from validated data since we don't store it
+        validated_data.pop('password_confirm')
+
+        # Use get_user_model().objects.create_user to create a new user
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+
+        # Create a token for the new user
+        Token.objects.create(user=user)
+
+        return user
